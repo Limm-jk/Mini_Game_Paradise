159531dd1a48680af15dcc206d669c9215622770
Limm-jk <201602057@cs-cnu.org>
2020-08-13 04:16:01 +0900

    [Add] React 라우팅 서비스 추가
    이게 무슨 Front야...

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home/Home';
 import FasterApp from './Faster_Game/FasterApp';
+import Menu from './Home/Menu';
 
 function App() {
   return (
     <div>
+      <Menu/>
       <Route exact path="/" component={Home}/>
+      {/* 위의 exact는 정확히 입력해야함을 뜻함. /같은경우 모두 사용되므로 밴 */}
       <Route path="/speed" component={FasterApp}/>
     </div>
   );
